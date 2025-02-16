508d8f0e020f4c667d4bd71f8276aaa211d14519
gyang <smartian@163.com>
2025-02-16 10:40:39 +0800

    format code

--- tgtrader/streamlit_pages/pages/option/hk_option_quotes.py
+++ tgtrader/streamlit_pages/pages/option/hk_option_quotes.py
@@ -8,12 +8,14 @@ from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid import JsCode
 from loguru import logger
 
+
 def get_option_expiration_date(stock_code: str):
     with FutuOptionGateway() as option_gateway:
         expiry_dates = option_gateway.get_option_expiration_date(
             stock_code)
 
     return expiry_dates
+
 
 def get_option_chain(stock_code: str, expiry_date: str):
     # 初始化FUTU期权网关
@@ -61,6 +63,7 @@ def get_option_chain(stock_code: str, expiry_date: str):
 
         return df
 
+
 def get_stock_price(stock_code: str):
     # 获取股票最新价格
     with FutuStockGateway() as stock_gateway:
@@ -70,6 +73,7 @@ def get_stock_price(stock_code: str):
             stock_price = stock_snapshot[0].last_price
 
     return stock_price
+
 
 def create_grid_options(df, stock_price=None, option_type='call'):
     """
@@ -82,7 +86,7 @@ def create_grid_options(df, stock_price=None, option_type='call'):
         Grid options for AG Grid
     """
     gb = GridOptionsBuilder.from_dataframe(df)
-    
+
     # 配置基础列宽
     column_widths = {
         "序号": 60,
@@ -102,14 +106,14 @@ def create_grid_options(df, stock_price=None, option_type='call'):
         "Vega": 70,
         "Rho": 70
     }
-    
+
     # 设置列宽和固定列
     for col, width in column_widths.items():
         if col in df.columns:
             pin_side = 'right' if (option_type == 'call' and col == '序号') else \
-                      'left' if (option_type == 'put' and col == '序号') else None
+                'left' if (option_type == 'put' and col == '序号') else None
             gb.configure_column(col, width=width, pinned=pin_side)
-    
+
     # 配置默认列属性
     gb.configure_default_column(
         resizable=True,
@@ -118,12 +122,12 @@ def create_grid_options(df, stock_price=None, option_type='call'):
         type=["numericColumn", "numberColumnFilter"],
         cellStyle={'text-align': 'right'}
     )
-    
+
     # 添加条件样式
     if stock_price:
         highlight_condition = f"params.data['行权价'] < {stock_price}" if option_type == 'call' \
             else f"params.data['行权价'] > {stock_price}"
-        
+
         js_code = f"""
         function(params) {{
             if ({highlight_condition}) {{
@@ -135,8 +139,9 @@ def create_grid_options(df, stock_price=None, option_type='call'):
         }}
         """
         gb.configure_grid_options(getRowStyle=JsCode(js_code))
-    
+
     return gb.build()
+
 
 def display_option_table(df, title, stock_price=None, option_type='call'):
     """
@@ -156,6 +161,7 @@ def display_option_table(df, title, stock_price=None, option_type='call'):
            allow_unsafe_jscode=True,
            fit_columns_on_grid_load=False)
 
+
 def prepare_option_display_data(options_df, stock_price=None, option_type='call'):
     """
     准备期权显示数据
@@ -171,7 +177,7 @@ def prepare_option_display_data(options_df, stock_price=None, option_type='call'
         'ask_price', 'ask_vol', 'open_interest', 'volume',
         'implied_volatility', 'delta', 'gamma', 'theta', 'vega', 'rho'
     ]].copy()
-    
+
     # 计算内在价值
     if stock_price:
         is_call = options_df['type'].iloc[0] == OptionType.CALL
@@ -179,11 +185,11 @@ def prepare_option_display_data(options_df, stock_price=None, option_type='call'
             lambda x: round(max(stock_price - x if is_call else x - stock_price, 0), 3))
     else:
         display_df['intrinsic_value'] = 0.0
-    
+
     # 设置序号
     display_df.index = range(1, len(display_df) + 1)
     display_df['序号'] = display_df.index
-    
+
     # 重命名列
     chinese_columns = {
         'strike_price': '行权价',
@@ -203,20 +209,21 @@ def prepare_option_display_data(options_df, stock_price=None, option_type='call'
         'rho': 'Rho'
     }
     display_df.rename(columns=chinese_columns, inplace=True)
-    
+
     # 定义基础列顺序
-    base_columns = ['序号', '行权价', '最新价', '内在价值', '买价', '买量', 
+    base_columns = ['序号', '行权价', '最新价', '内在价值', '买价', '买量',
                     '卖价', '卖量', '未平仓数', '成交量', '隐波',
                     'Delta', 'Gamma', 'Theta', 'Vega', 'Rho']
-    
+
     # 如果是看涨期权，反转除序号外的列顺序
     if option_type == 'call':
         reversed_columns = base_columns[1:][::-1]  # 反转除序号外的所有列
         col_order = ['序号'] + reversed_columns
     else:
         col_order = base_columns
-    
+
     return display_df[col_order]
+
 
 def display_option_chain(call_options, put_options, stock_price=None):
     """
@@ -227,17 +234,19 @@ def display_option_chain(call_options, put_options, stock_price=None):
         stock_price: Current stock price
     """
     # 准备数据
-    call_display = prepare_option_display_data(call_options, stock_price, 'call')
+    call_display = prepare_option_display_data(
+        call_options, stock_price, 'call')
     put_display = prepare_option_display_data(put_options, stock_price, 'put')
-    
+
     # 创建两列布局显示数据
     col_call, col_put = st.columns(2)
-    
+
     with col_call:
         display_option_table(call_display, "看涨期权", stock_price, 'call')
-    
+
     with col_put:
         display_option_table(put_display, "看跌期权", stock_price, 'put')
+
 
 def run():
     """
@@ -313,7 +322,8 @@ def run():
 
             # 显示股票最新价
             if stock_price:
-                st.metric(label=f"{stock_code} 最新价", value=f"{stock_price:.3f}")
+                st.metric(label=f"{stock_code} 最新价",
+                          value=f"{stock_price:.3f}")
 
             # 分离看涨和看跌期权
             call_options = df[df['type'] == OptionType.CALL].sort_values(
